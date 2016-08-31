b115d02624159dc2a427b9fed5b7cd0ef6372629
Nicholas Calugar <ncalugar@pivotal.io>
2016-08-31 11:58:56 -0700

    Support deleting apps with space in name

--- commands/delete.go
+++ commands/delete.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/cloudfoundry/cli/plugin"
@@ -11,9 +12,13 @@ import (
 
 func Delete(cliConnection plugin.CliConnection, args []string) {
 	appName := args[1]
+
+	urlValues := url.Values{}
+	urlValues.Add("names", args[1])
+
 	fmt.Printf("Deleting app %s...\n", appName)
 
-	rawOutput, _ := cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("/v3/apps?names=%s", appName))
+	rawOutput, _ := cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("/v3/apps?%s", urlValues.Encode()))
 	apps := V3AppsModel{}
 	output := strings.Join(rawOutput, "")
 	json.Unmarshal([]byte(output), &apps)
--- commands/delete_test.go
+++ commands/delete_test.go
@@ -25,6 +25,14 @@ var _ = Describe("Delete", func() {
 					}
 				]
 			}`
+		searchResultWithSpace = `{
+				"resources": [
+					{
+						"guid": "feed-dead-beef-with-space",
+						"name": "my app"
+					}
+				]
+			}`
 		searchError error
 
 		deleteResult = `{
@@ -37,7 +45,11 @@ var _ = Describe("Delete", func() {
 		fakeCliConnection.CliCommandWithoutTerminalOutputStub = func(args ...string) ([]string, error) {
 			if reflect.DeepEqual(args, []string{"curl", "/v3/apps?names=my-app"}) {
 				return []string{searchResult}, searchError
+			} else if reflect.DeepEqual(args, []string{"curl", "/v3/apps?names=my+app"}) {
+				return []string{searchResultWithSpace}, searchError
 			} else if reflect.DeepEqual(args, []string{"curl", "/v3/apps/feed-dead-beef", "-X", "DELETE"}) {
+				return []string{deleteResult}, deleteError
+			} else if reflect.DeepEqual(args, []string{"curl", "/v3/apps/feed-dead-beef-with-space", "-X", "DELETE"}) {
 				return []string{deleteResult}, deleteError
 			}
 			return []string{""}, nil
@@ -51,6 +63,19 @@ var _ = Describe("Delete", func() {
 
 		Expect(output[0]).To(Equal("Deleting app my-app..."))
 		Expect(output[1]).To(Equal("OK"))
+	})
+
+	Context("When the app name has a space", func() {
+		It("deletes the app", func() {
+			commandArgsWithSpace := []string{"v3-delete", "my app"}
+
+			output := io_helpers.CaptureOutput(func() { Delete(fakeCliConnection, commandArgsWithSpace) })
+			Expect(fakeCliConnection.CliCommandWithoutTerminalOutputArgsForCall(1)).
+				To(Equal([]string{"curl", "/v3/apps/feed-dead-beef-with-space", "-X", "DELETE"}))
+
+			Expect(output[0]).To(Equal("Deleting app my app..."))
+			Expect(output[1]).To(Equal("OK"))
+		})
 	})
 
 	Context("The app fails to delete", func() {
