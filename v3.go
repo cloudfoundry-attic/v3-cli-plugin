d3f1efc4ba06ff66be4193ad1fe27489af89d7b1
Utako Ueda <uueda@pivotal.io>
2016-09-22 14:53:05 -0700

    Bump plugin version

--- v3.go
+++ v3.go
@@ -74,7 +74,7 @@ func (v3plugin *V3Plugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 5,
-			Build: 3,
+			Build: 4,
 		},
 		Commands: []plugin.Command{
 			{
